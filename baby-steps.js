73834904426664ebf0cf283a60a54d83e75f0834
Yuna Emiya <yuna-emiya@outlook.com>
2026-01-09 12:31:15 +0100

    finished for loop in function and call the function

--- baby-steps.js
+++ baby-steps.js
@@ -2,9 +2,12 @@ const array = [];
 
 function insertArguments(){
  for(let i = 2; i < Process.argv.length; i++){
-
+    const currentArgument = Value(process.argv[i])
+    array.push(currentArgument)
  }
 }
+
+insertArguments();
 
 for (let i = 0; i < array.length; i++){
 
